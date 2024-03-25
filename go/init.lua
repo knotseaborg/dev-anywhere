ccba6d52cb90ebe161f9fb74622467e5ac368389
Reuben Sinha <reubensinha7@gmail.com>
2024-03-25 23:36:17 +0900

    Initiated
    Installed Go & Neovim
    Got some plugins running with lazy.vim and gopls as the LSP.

---
+++ go/init.lua
@@ -0,0 +1,51 @@
+-- Sets up lazy-vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Add plugins here
+require("lazy").setup({
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+})
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = { 'lua', 'go'},  -- List of parsers to install
+    highlight = {
+        enable = true,  -- Enable Treesitter syntax highlighting
+    },
+    indent = {
+        enable = true,  -- Enable Treesitter-based indenting
+    },
+}
+
+require("lspconfig").gopls.setup({}) -- Setup and start lsp server
+require("go").setup() -- Setup and start go.nvim
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
