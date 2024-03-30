e6c0635fb832ca47b1ac9d44c7fa2aba24257cbc
Reuben Sinha <reubensinha7@gmail.com>
2024-03-30 23:50:03 +0900

    Refactoring
    Just some refactoring

---
+++ go/lua/plugins.lua
@@ -0,0 +1,52 @@
+-- Sets up lazy-vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Add plugins here
+require("lazy").setup({
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+      require("lspconfig").gopls.setup({})
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'lua', 'go', 'vimdoc', 'vim'}
+      })
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+},{
+    "folke/tokyonight.nvim",
+    config = function()
+      vim.g.tokyonight_style = "storm"
+      vim.cmd("colorscheme tokyonight")
+    end,
+    lazy = false,
+    priority = 1000,
+    opts = {},
+})
+
+-- Autocmds for Go
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
